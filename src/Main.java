fa94127c70f377367bc677e66925720725a8bb9d
F99 <farajiomar99@gmail.com>
2025-09-30 10:01:13 +0100

    Main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import DAO.ConnectionDAO;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test the database connection
+        // ConnectionDAO.getConnection();
+    }
+}
