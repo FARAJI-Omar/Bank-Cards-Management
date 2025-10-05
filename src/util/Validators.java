6d4e42eac80919023252c22782a8f29a3d5670a0
F99 <farajiomar99@gmail.com>
2025-10-05 19:28:13 +0100

    refactor: remove commented-out operation amount validation method

--- src/util/Validators.java
+++ src/util/Validators.java
@@ -16,9 +16,4 @@ public class Validators {
         String nameRegex = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$";
         return name != null && name.matches(nameRegex);
     }
-
-//    // Operation amount validation
-//    public static boolean isValidOperationAmount(double amount) {
-//        return amount > 0 && amount <= 10000; // Max withdraw/purchase/online pay limit of 10,000 DH
-//    }
 }
